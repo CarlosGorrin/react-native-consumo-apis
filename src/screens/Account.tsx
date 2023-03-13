d0674ea0b58f0c400f1edc710488473a4c000b91
CarlosGorrin <carlos.gorrin@gmail.com>
2023-03-13 03:15:08 -0300

    adding ts & navigation

---
+++ src/screens/Account.tsx
@@ -0,0 +1,10 @@
+import { Text, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+
+export default function AccountScreen() {
+    return (
+      <SafeAreaView>
+        <Text>Account</Text>
+      </SafeAreaView>
+    )
+}
---
+++ src/screens/Pokedex.tsx
@@ -0,0 +1,10 @@
+import { Text, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+
+export default function PokedexScreen() {
+    return (
+      <SafeAreaView>
+        <Text>POKEDEX</Text>
+      </SafeAreaView>
+    )
+}
